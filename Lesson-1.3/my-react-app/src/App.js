19b099764cf8c917aace70465c803b3e7b3dd698
cloudshare360 <79122392+cloudshare360@users.noreply.github.com>
2024-04-10 22:14:54 +0000

    1. Added Good Morning/ Good Evening based on user Time 2: Added variable adjective and placing in JSX page

--- Lesson-1.3/my-react-app/src/App.js
+++ Lesson-1.3/my-react-app/src/App.js
@@ -2,19 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let isMorning = (new Date()).getHours() < 12;
+  let greetingElement = isMorning?
+                        <h3> Good Morning! </h3>
+                        : <h3> Good Evening! </h3>
+
+  let adjective  = "cool"
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {greetingElement}
         </p>
+        <p> This is so {adjective} 
+          </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
+         
           Learn React
         </a>
       </header>
